8ae6f7dc3729cfdc39ceaa2cd693df88c5952498
rad-koc <rad-koc@yandex.ru>
2025-03-17 20:02:32 +0300

    use clang as compiler in the scripts

--- tools/_test_compile.bat
+++ tools/_test_compile.bat
@@ -1,2 +1,3 @@
 REM D:\Projects\LLVM\llvm_17\build\Debug\bin\clang-cl.exe -o expr.o "D:\Projects\LLVM\Learning\CompilerStructure\src\runtime.cpp"
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\Hostx64\x64\cl.exe" expr.obj "D:\Projects\LLVM\Learning\CompilerStructure\src\runtime.c"
+REM "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\Hostx64\x64\cl.exe" expr.obj "D:\Projects\LLVM\Learning\CompilerStructure\src\runtime.c"
+"D:\Projects\LLVM\llvm_17\build\Debug\bin\clang-cl.exe" expr.obj "D:\Projects\LLVM\Learning\CompilerStructure\src\runtime.c"
